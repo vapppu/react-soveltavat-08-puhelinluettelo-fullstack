a20a25c302e007012978b5fc60a8a1575636f506
Veera Hiltunen <veera.hiltunen@gmail.com>
2023-11-21 20:38:34 +0200

    Add requirements for phone number for validation

--- backend/models/person.js
+++ backend/models/person.js
@@ -24,8 +24,14 @@ const personSchema = new mongoose.Schema({
   number: {
     type: String,
     minlength: 8,
+    validate: {
+        validator: function(v) {
+          return /\d{2,3}-\d+/.test(v);
+        },
+        message: props => `${props.value} is not a valid phone number!`
+    },
     required: true
-  },
+}
 });
 
 personSchema.set("toJSON", {
